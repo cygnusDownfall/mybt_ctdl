714bd58266db457b187f6e5f001afe41abf995e8
CygnusDownfall <phapsuhacam112@gmail.com>
2022-10-01 22:46:36 +0700

    first commit

---
+++ 020101125/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _020101125
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
