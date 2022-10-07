20bb6356132c6ec77531f897f674bc0535fc48db
CygnusDownfall <phapsuhacam112@gmail.com>
2022-10-07 20:27:23 +0700

    7/10/2022

--- 020101125/DSSV.cs
+++ 020101125/DSSV.cs
@@ -1,11 +1,42 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Text;
+using System.IO;
 
 namespace _020101125
 {
     public  class DSSV
     {
+        List<SINHVIEN> ds;
+        public int count { get => ds.Count; }
+        public List<SINHVIEN> DanhsachSV { get => ds; set { } }  
+        public DSSV()
+        {
+            ds = new List<SINHVIEN> ();
 
+        }
+        void Readfromfile(string filepath)
+        {
+            using (StreamReader rd = new StreamReader(filepath))
+            {
+                int i = 0;
+                while (rd.EndOfStream)
+                {
+
+                    ds[i] = new SINHVIEN();
+                    i++;
+                }
+            }
+        }
+        void Writefile(string filepath)
+        {
+            using (StreamWriter wt = new StreamWriter(filepath))
+            {
+
+            }
+        }
+        void SapXep()
+        {
+
+        }
     }
 }
--- 020101125/SINHVIEN.cs
+++ 020101125/SINHVIEN.cs
@@ -6,5 +6,22 @@ namespace _020101125
 {
     public class SINHVIEN
     {
+        int sbd, toan, van, anh;
+        public int SBD { get { return sbd; }  }
+        public int Toan { get { return toan; } }   
+        public int Van { get { return van; } }  
+        public int Anh { get { return anh; } }  
+        public SINHVIEN()
+        {
+
+        }
+        public SINHVIEN(int Sbd,int dtoan,int dvan,int danh)
+        {
+            sbd = Sbd;
+            toan = dtoan;
+            van = dvan;
+            anh = danh;
+
+        }
     }
 }
